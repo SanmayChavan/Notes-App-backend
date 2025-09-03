6cc76344d330484fd563c027a785289123618ef4
Sanmay Chavan <sanmaychavan22@gmail.com>
2025-09-03 10:33:47 +0530

    Prepare backend for Vercel deployment: export app, fix vercel.json

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
+// import express from 'express'
+// import router from './src/routes/notesRoutes.js'
+// import { connectDB } from './src/config/db.js'
+// import cors from 'cors'
+// import dotenv from 'dotenv'
+
+// dotenv.config()
+
+
+// const app = express()
+
+// const PORT = process.env.PORT || 5001
+
+
+
+// //middleware
+// app.use(cors())
+// app.use(express.json())
+// app.use('/api/notes', router)
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the Notes API backend!')
+// })
+
+
+
+// connectDB().then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`server running at PORT:${PORT}`)
+//     })
+
+// })
+
+
 import express from 'express'
 import router from './src/routes/notesRoutes.js'
 import { connectDB } from './src/config/db.js'
@@ -6,23 +40,16 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-
 const app = express()
 
-const PORT = process.env.PORT || 5001
-
-
-//middleware
 app.use(cors())
 app.use(express.json())
 app.use('/api/notes', router)
 
-
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`server running at PORT:${PORT}`)
-    })
-
+app.get('/', (req, res) => {
+  res.send('Welcome to the Notes API backend!')
 })
 
+connectDB().catch(console.error)
 
+export default app
